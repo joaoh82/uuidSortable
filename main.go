58e05193331d068ddd3226876a989a2f32550e1f
Joao Henrique Machado Silva <joaoh82@gmail.com>
2017-02-14 22:57:07 -0200

    generating 1000 ids in less then a second

--- main.go
+++ main.go
@@ -3,53 +3,16 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
-
-type empty interface{}
-type semaphore chan empty
-
-// acquire n resources
-func (s semaphore) P(n int) {
-	e := empty{}
-	for i := 0; i < n; i++ {
-		s <- e
-	}
-}
-
-// release n resources
-func (s semaphore) V(n int) {
-	for i := 0; i < n; i++ {
-		<-s
-	}
-}
-
-/* mutexes */
-
-func (s semaphore) Lock() {
-	s.P(1)
-}
-
-func (s semaphore) Unlock() {
-	s.V(1)
-}
-
-/* signal-wait */
-
-func (s semaphore) Signal() {
-	s.V(1)
-}
-
-func (s semaphore) Wait(n int) {
-	s.P(n)
-}
 
 const numIds = 1000
 
 func main() {
-	var wg sync.WaitGroup
+	start := time.Now()
 
+	var wg sync.WaitGroup
 	wg.Add(numIds)
-	sem := make(semaphore, numIds)
 	for i := 0; i < numIds; i++ {
 		go func(i int) {
 			defer wg.Done()
@@ -58,9 +21,9 @@ func main() {
 				panic(err)
 			}
 			fmt.Printf("UUID %v: %v\n", i, u.String())
-			sem.Signal()
 		}(i)
 	}
-	sem.Wait(numIds)
 	wg.Wait()
+	elapsed := time.Since(start)
+	fmt.Printf("1000 ids took = %f\n", elapsed.Seconds())
 }
